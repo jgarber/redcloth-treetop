4117b592439f4b88e5ee932be0c0de70195d80ca
Jason Garber <jg@jasongarber.com>
2009-06-14 16:20:05 -0400

    Mspace specs.

--- spec/redcloth/parser/common_parser_spec.rb
+++ spec/redcloth/parser/common_parser_spec.rb
@@ -16,6 +16,16 @@ module RedCloth
       describe "mspace rule" do
         before(:each) { @parser.root = :mspace }
         
+        it "should parse space" do
+          lambda { parse(" ") }.should_not raise_error
+        end
+        it "should parse tab" do
+          lambda { parse("\t") }.should_not raise_error
+        end
+        it "should parse space tab" do
+          lambda { parse(" \t") }.should_not raise_error
+        end
+        
         it "should not parse double newline" do
           lambda { parse("\n\n") }.should raise_error(RedCloth::Parser::SyntaxError)
         end
