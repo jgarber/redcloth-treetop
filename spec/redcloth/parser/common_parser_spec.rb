06fc3aac4ea0e256afda51d3df064f029f609b1e
Jason Garber <jg@jasongarber.com>
2009-06-14 15:00:42 -0400

    Added some rules to CommonParser and figured out a way to spec them too.

---
+++ spec/redcloth/parser/common_parser_spec.rb
@@ -0,0 +1,44 @@
+require File.dirname(__FILE__) + "/../../spec_helper"
+require 'redcloth/parser/common'
+
+module RedCloth
+  module Parser
+    describe CommonParser do
+      
+      before :each do
+        @parser = CommonParser.new
+      end
+      
+      def parse(string)
+        @parser.parse_or_fail(string)
+      end
+      
+      describe "mspace rule" do
+        before(:each) { @parser.root = :mspace }
+        
+        it "should not parse double newline" do
+          lambda { parse("\n\n") }.should raise_error(RedCloth::Parser::SyntaxError)
+        end
+        
+      end
+      
+      describe "mtext rule" do
+        before(:each) { @parser.root = :mtext }
+        
+        it "should parse mtext" do
+          ["some text", "text", "a little text.", "some\ntext"].each do |text|
+            lambda { parse(text) }.should_not raise_error
+          end
+        end
+        
+        it "should not parse non-mtext" do
+          ["some text ", " text", "text\n", "text\n\n", "some\n\ntext"].each do |text|
+            lambda { parse(text) }.should raise_error(RedCloth::Parser::SyntaxError)
+          end
+        end
+
+      end
+      
+    end
+  end
+end
