f0d16236ad54c739d37ed6adcec0eb4c31da2382
Jason Garber <jg@jasongarber.com>
2009-06-15 12:13:31 -0400

    Moved strong rules into their own StrongParser.

---
+++ spec/redcloth/parser/inline/strong_parser_spec.rb
@@ -0,0 +1,67 @@
+require File.dirname(__FILE__) + "/../../../spec_helper"
+require 'redcloth/parser/inline/strong'
+
+module RedCloth
+  module Parser
+    module Inline
+      describe StrongParser do
+        before :each do
+          @parser = StrongParser.new
+        end
+      
+        def parse(string)
+          @parser.parse_or_fail(string)
+        end
+      
+        it "should parse a strong phrase" do
+          parse("*don't you dare!*").to_sexp.should ==
+            [:strong, {}, ["don't you dare!"]]
+        end
+      
+        it "should allow asterisks as words in a strong phrase" do
+          parse("*2 * 7*").to_sexp.should ==
+            [:strong, {}, ["2 * 7"]]
+        end
+
+        it "should allow a strong phrase to contain asterisks in a word" do
+          parse("*veg*an*").to_sexp.should ==
+            [:strong, {}, ["veg*an"]]
+        end
+      
+        describe "strong_word rule" do
+          before(:each) { @parser.root = :strong_word }
+        
+          it "should parse a normal word" do
+            lambda { parse("word") }.should_not raise_error
+          end
+          it "should parse a word with an asterisk in it" do
+            parse("veg*an")
+          end
+          it "should parse a lone asterisk" do
+            parse("*")
+          end
+          it "should include leading asterisk in a word" do
+            parse("*yow").should == "*yow"
+          end
+          it "should not include trailing asterisk in a word if the next char is a space" do
+            @parser.consume_all_input = false
+            parse("yow* ").should == "yow"
+          end
+          it "should include trailing asterisk in a word if the next char is an asterisk" do
+            @parser.consume_all_input = false
+            parse("yow**").should == "yow*"
+          end
+          it "should not include trailing asterisk in a word if the next char is an underscore" do
+            @parser.consume_all_input = false
+            parse("yow*_").should == "yow"
+          end
+          it "should not include trailing asterisk in a word if the next char is a question mark" do
+            @parser.consume_all_input = false
+            parse("yow*?").should == "yow"
+          end
+        end
+           
+      end
+    end
+  end
+end
--- spec/redcloth/parser/inline_parser_spec.rb
+++ spec/redcloth/parser/inline_parser_spec.rb
@@ -17,11 +17,6 @@ module RedCloth
           ["Just plain text."]
       end
       
-      it "should parse a strong phrase" do
-        parse("*don't you dare!*").to_sexp.should ==
-          [[:strong, {}, ["don't you dare!"]]]
-      end
-      
       it "should parse a phrase with asterisks that is not a strong phrase" do
         parse("yes * we * can").to_sexp.should ==
          ["yes * we * can"]
@@ -32,19 +27,19 @@ module RedCloth
          ["yeah *that's * it!"]
       end
       
-      it "should allow asterisks as words in a strong phrase" do
-        parse("compute *2 * 7*").to_sexp.should ==
-          ["compute ", [:strong, {}, ["2 * 7"]]]
+      it "should parse a strong phrase" do
+        parse("*strong phrase*").to_sexp.should ==
+          [[:strong, {}, ["strong phrase"]]]
+      end
+      
+      it "should parse a strong phrase surrounded by plain text" do
+        parse("plain *strong phrase* plain").to_sexp.should ==
+          ["plain ", [:strong, {}, ["strong phrase"]], " plain"]
       end
       
       it "should allow a strong phrase at the end of a sentence before punctuation" do
         parse("Are you *veg*an*?").to_sexp.should ==
           ["Are you ", [:strong, {}, ["veg*an"]], "?"]
-      end
-      
-      it "should allow a strong phrase to contain asterisks in a word" do
-        parse("There are *veg*an* options.").to_sexp.should ==
-          ["There are ", [:strong, {}, ["veg*an"]], " options."]
       end
       
       it "should parse an emphasized phrase" do
@@ -78,39 +73,6 @@ module RedCloth
             "this is ",
             [:bold, {}, ["bold"]],
             " see"]]]
-      end
-      
-      describe "strong_word rule" do
-        before(:each) { @parser.root = :strong_word }
-        
-        it "should parse a normal word" do
-          lambda { parse("word") }.should_not raise_error
-        end
-        it "should parse a word with an asterisk in it" do
-          parse("veg*an")
-        end
-        it "should parse a lone asterisk" do
-          parse("*")
-        end
-        it "should include leading asterisk in a word" do
-          parse("*yow").should == "*yow"
-        end
-        it "should not include trailing asterisk in a word if the next char is a space" do
-          @parser.consume_all_input = false
-          parse("yow* ").should == "yow"
-        end
-        it "should include trailing asterisk in a word if the next char is an asterisk" do
-          @parser.consume_all_input = false
-          parse("yow**").should == "yow*"
-        end
-        it "should not include trailing asterisk in a word if the next char is an underscore" do
-          @parser.consume_all_input = false
-          parse("yow*_").should == "yow"
-        end
-        it "should not include trailing asterisk in a word if the next char is a question mark" do
-          @parser.consume_all_input = false
-          parse("yow*?").should == "yow"
-        end
       end
       
       describe "bold_word rule" do
